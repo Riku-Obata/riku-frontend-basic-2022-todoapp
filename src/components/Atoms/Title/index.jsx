282667ba7b3c2b944d4c81a6e7849882bd90278c
Riku <riku.o-222-my.friend@ezweb.ne.jp>
2023-09-24 13:18:03 +0000

    Atoms/Title まねる　所まで

---
+++ src/components/Atoms/Title/index.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+import React from "react";
+
+export const Title = () => {
+  return <StyledText>SIMPLE TODO APP</StyledText>;
+}
+
+const StyledText = styled.div`
+  color: white;
+`;
