302b90c3ec04274040eaf2ad348336860c49cc54
Riku <riku.o-222-my.friend@ezweb.ne.jp>
2023-10-02 08:30:30 +0000

    imgの追加、hoverの作成

--- src/components/Atoms/EditButton/index.jsx
+++ src/components/Atoms/EditButton/index.jsx
@@ -3,10 +3,32 @@ import pencil from "../../../assets/svg/pencil.svg"
 import React from "react";
 import COLOR from "../../../variables/color";
 
-export const EditButton = (props) => {
-    return <Button onClick={props.onClick}>{props.label}</Button>;
+
+export const EditButton = ({onClick}) => {
+    return <Button onClick={onClick}><img src={pencil} className="img-size"/></Button>;
 }
+export default EditButton;
 
 const Button = styled.button`
-  color: ${COLOR.LIGHT_GRAY}
+ display: flex;
+ background: none;
+ width: 20px;
+ height: 20px;
+ border-radius: 50%;
+ padding: 0;
+ border: 0;
+ transition: 0.2s;
+ justify-content: center;
+ align-items: center;
+ flex-shrink: 0;
+
+ .img-size{
+    width: 100%;
+    height: 100%;
+ }
+ 
+ :hover{
+   background-color: ${COLOR.LIGHT_GRAY_TRANSLUCENT};
+ }
 `;
+
