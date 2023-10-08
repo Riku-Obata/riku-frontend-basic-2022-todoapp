8885b715df5ac62cd24bb899d808a6f60307691c
Riku <riku.o-222-my.friend@ezweb.ne.jp>
2023-10-08 15:37:36 +0000

    Button 画像、テキストの追加

--- src/components/Atoms/AddTaskButton/index.jsx
+++ src/components/Atoms/AddTaskButton/index.jsx
@@ -5,8 +5,13 @@ import COLOR from "../../../variables/color";
 
 
 export const AddTaskButton = ({onClick}) => {
-    return <Button onClick={onClick}><img src={plus} className="img-size"/></Button>;
+    return <Button onClick={onClick}><img src={plus}/>タスクを追加</Button>;
 }
 export default AddTaskButton;
 
-const Button = styled.plusButton
+const Button = styled.button`
+ display: flex;
+ padding: 2px 6px;
+ align-items: center;
+ gap: 10px;
+`
