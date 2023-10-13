82146c791e93019d12b7e60c7e97c7cc33d95ae3
Riku-Obata <riku.o-222-my.friend@ezweb.ne.jp>
2023-10-13 21:40:10 +0900

    EditButton コードの修正

--- src/components/Atoms/EditButton/index.jsx
+++ src/components/Atoms/EditButton/index.jsx
@@ -1,34 +1,37 @@
 import styled from "styled-components";
-import pencil from "../../../assets/svg/pencil.svg"
+import pencil from "../../../assets/svg/pencil.svg";
 import React from "react";
 import COLOR from "../../../variables/color";
 
-
-export const EditButton = ({onClick}) => {
-    return <Button onClick={onClick}><img src={pencil} className="img-size"/></Button>;
-}
+export const EditButton = ({ onClick }) => {
+  return (
+    <Button onClick={onClick}>
+      <img src={pencil} className="img-size" />
+    </Button>
+  );
+};
 export default EditButton;
 
 const Button = styled.button`
- display: flex;
- background: none;
- width: 20px;
- height: 20px;
- border-radius: 50%;
- padding: 0;
- border: 0;
- transition: 0.2s;
- justify-content: center;
- align-items: center;
- flex-shrink: 0;
+  display: flex;
+  background: none;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  padding: 0;
+  border: 0;
+  transition: 0.2s;
+  justify-content: center;
+  align-items: center;
+  flex-shrink: 0;
 
- .img-size{
+  .img-size {
     width: 100%;
     height: 100%;
- }
- 
- :hover{
-   background-color: ${COLOR.LIGHT_GRAY_TRANSLUCENT};
-   cursor: pointer;
- }
+  }
+
+  :hover {
+    background-color: ${COLOR.LIGHT_GRAY_TRANSLUCENT};
+    cursor: pointer;
+  }
 `;
