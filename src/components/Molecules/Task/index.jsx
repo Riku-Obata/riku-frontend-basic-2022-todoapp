a3081742cbf26985b3b6596020e1435b8fcc160c
Riku-Obata <riku.o-222-my.friend@ezweb.ne.jp>
2023-10-25 14:44:29 +0900

    6行目　exportの中身の記述

---
+++ src/components/Molecules/Task/index.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import React, { useState } from "react";
+import COLOR from "../../../variables/color";
+import TEXT from "../../../variables/texts";
+
+export const Task = ({
+  onTaskNameChange,
+  onTaskComplete,
+  taskName,
+  defaultIsEditing,
+}) => {
+  return;
+};
+export default Task;
