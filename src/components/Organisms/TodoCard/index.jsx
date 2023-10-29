e85ef4f7203a83dc27d63ae536dace5a099d4922
Riku-Obata <riku.o-222-my.friend@ezweb.ne.jp>
2023-10-29 14:02:32 +0900

    その他の関数とreturnの一部作成

--- src/components/Organisms/TodoCard/index.jsx
+++ src/components/Organisms/TodoCard/index.jsx
@@ -6,8 +6,24 @@ import Input from "../../Atoms/Input";
 import TEXT from "../../../variables/texts";
 import FONTFAMILY from "../../../variables/font_family";
 import COLOR from "../../../variables/color";
+import AddTaskButton from "../../Atoms/AddTaskButton";
 
 export const TodoCard = () => {
-  return;
+  const [taskList, setTaskLIst] = useState([]);
+
+  const onAddTaskButtonClick = () => {
+    setTaskLIst();
+  };
+
+  const onTaskComplete = (index) => {};
+
+  const onTaskNameChange = (value, index) => {};
+
+  return (
+    <StyledWrapper>
+      <AddTaskButton onClick={onAddTaskButtonClick} />
+      <StyledTaskList></StyledTaskList>
+    </StyledWrapper>
+  );
 };
 export default TodoCard;
