482b1cc4590d54347e9317c654b485cce6769808
Riku-Obata <riku.o-222-my.friend@ezweb.ne.jp>
2023-11-21 23:18:03 +0900

    PC版　アラート表示の完成

--- src/components/Atoms/Alert/index.jsx
+++ src/components/Atoms/Alert/index.jsx
@@ -4,24 +4,42 @@ import React from "react";
 import COLOR from "../../../variables/color";
 import FONTFAMILY from "../../../variables/font_family";
 import TEXT from "../../../variables/texts";
-import visible from "./index.stories";
 
-export const Alert = () => {
-  return <TaskAlert isActive={visible}>text</TaskAlert>;
+export const Alert = ({ visible, errorText }) => {
+  return (
+    <TaskAlert visible={visible}>
+      <TextAlert>{errorText}</TextAlert>
+    </TaskAlert>
+  );
 };
 export default Alert;
 
 const TaskAlert = styled.div`
-  visibility: ${(visible) => (visible.isActive ? visible : hidden)};
   display: flex;
-  width: 400px;
-  padding: 10px 20px;
   flex-direction: column;
   align-items: flex-start;
+  position: absolute;
+  top: 80px;
+  left: 50%;
+  width: 100%;
+  max-width: 400px;
+  padding: 10px 20px;
   gap: 10px;
   border-radius: 4px;
+  transition: 0.5s;
+  transform: translateX(-50%) translateY(-40px);
+  opacity: 0;
   background: ${COLOR.RED};
-  color: ${COLOR.WHITE};
   font-family: ${FONTFAMILY.NOTO_SANS};
+
+  ${(props) =>
+    props.visible
+      ? `{ opacity: 1;
+       transform: translateX(-50%) translateY(0) }`
+      : ``}
+`;
+
+const TextAlert = styled.div`
+  color: ${COLOR.WHITE};
   ${TEXT.S};
 `;
