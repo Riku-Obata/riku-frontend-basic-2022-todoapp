d404b932a6affd4740a1a31b316fa07e7771f62e
Riku-Obata <riku.o-222-my.friend@ezweb.ne.jp>
2023-11-23 22:01:52 +0900

    作成したコンポーネントをsrc index.jsxに設置

--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
 import MainPage from "./components/Pages/MainPage";
+import AlertManager from "./components/Organisms/AlertManager";
+import { AlertHandlerProvider } from "./contexts/alert_handler";
 
-ReactDOM.render(<MainPage />, document.getElementById("app"));
+ReactDOM.render(
+  <AlertHandlerProvider>
+    <MainPage />
+    <AlertManager />
+  </AlertHandlerProvider>,
+  document.getElementById("app")
+);
