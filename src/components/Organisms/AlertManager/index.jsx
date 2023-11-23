24490017e0ec24eef48b3928f2534e5b135224f0
Riku-Obata <riku.o-222-my.friend@ezweb.ne.jp>
2023-11-23 21:51:55 +0900

    Organisms/AlertManager の作成

--- src/components/Organisms/AlertManager/index.jsx
+++ src/components/Organisms/AlertManager/index.jsx
@@ -1,13 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useAlertHandlerContext } from "path/to/contexts/alert_handler";
 import Alert from "../../Atoms/Alert";
 
 export const AlertManager = () => {
   const AlertHandlerContext = useAlertHandlerContext();
 
-  //AlertHandlerContextから直接値が取り出せる
-  console.log(AlertHandlerContext.visible);
-  AlertHandlerContext.setAlert("message"); //Alertの表示
-  return <Alert />;
+  useEffect(() => {
+    if (AlertHandlerContext.visible === true) {
+      AlertHandlerContext.setAlert("message");
+    }
+
+    setTimeout(() => {
+      AlertHandlerContext.closeAlert("message");
+    }, 5000);
+  }, []);
+
+  return (
+    <Alert
+      visible={AlertHandlerContext.visible}
+      errorText={AlertHandlerContext.errorText}
+    />
+  );
 };
 export default AlertManager;
