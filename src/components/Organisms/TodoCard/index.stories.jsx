7cbffb9d27b4be6bd17f8516c55b414bce7ad380
Riku-Obata <riku.o-222-my.friend@ezweb.ne.jp>
2023-11-28 14:59:12 +0900

    StorybookのAlertの表示

--- src/components/Organisms/TodoCard/index.stories.jsx
+++ src/components/Organisms/TodoCard/index.stories.jsx
@@ -1,19 +1,5 @@
 import { TodoCard } from "./index";
-import { AlertHandlerProvider } from "../../../contexts/alert_handler";
-import AlertManager from "../../Organisms/AlertManager";
-import { Template } from "webpack";
 
 export default { component: TodoCard };
 
-export const Default = Template.bind({});
-Default.args = {};
-Default.decorators = [
-  (Story) => (
-    <div>
-      <AlertHandlerProvider>
-        <AlertManager />
-        <Story />
-      </AlertHandlerProvider>
-    </div>
-  ),
-];
+export const Default = {};
