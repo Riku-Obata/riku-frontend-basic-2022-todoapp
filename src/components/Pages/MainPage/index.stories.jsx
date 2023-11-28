12ff644ecdce4cea2fcff429d0fecc0cc8b05a4a
Riku-Obata <riku.o-222-my.friend@ezweb.ne.jp>
2023-11-28 16:29:21 +0900

    Chromaticのエラー修正

--- src/components/Pages/MainPage/index.stories.jsx
+++ src/components/Pages/MainPage/index.stories.jsx
@@ -1,18 +1,5 @@
 import { MainPage } from "./index";
-import { AlertHandlerProvider } from "../../../contexts/alert_handler";
-import AlertManager from "../../Organisms/AlertManager";
-import { Template } from "webpack";
+
 export default { component: MainPage };
 
-export const Default = Template.bind({});
-Default.args = {};
-Default.decorators = [
-  (Story) => (
-    <div>
-      <AlertHandlerProvider>
-        <AlertManager />
-        <Story />
-      </AlertHandlerProvider>
-    </div>
-  ),
-];
+export const Default = {};
